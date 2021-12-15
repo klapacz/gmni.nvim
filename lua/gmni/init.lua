4d38d017671f7bd217a30a32ddd99f91c29c757c
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-12-15 01:32:55 +0100

    use vim.schedule_wrap

--- lua/gmni/init.lua
+++ lua/gmni/init.lua
@@ -6,17 +6,15 @@ function M.get(url)
 	Job:new({
 		command = 'gmni',
 		args = { '-j', 'always', url },
-		on_exit = function(j, result)
+		on_exit = vim.schedule_wrap(function(j, result)
 			local lines = j:result()
 
-			vim.schedule(function ()
-				-- create new tab and get buffer id
-				api.nvim_command("tabnew")
-				local bufnr = api.nvim_win_get_buf(0)
+			-- create new tab and get buffer id
+			api.nvim_command("tabnew")
+			local bufnr = api.nvim_win_get_buf(0)
 
-				api.nvim_buf_set_lines(bufnr, 0, #lines, false, lines)
-			end)
-		end,
+			api.nvim_buf_set_lines(bufnr, 0, #lines, false, lines)
+		end),
 	}):start()
 end
 
