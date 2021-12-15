47aaffddb5b73c703feeecc36b0fc4361bc128fb
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-12-16 00:36:51 +0100

    keybindings

--- lua/gmni/init.lua
+++ lua/gmni/init.lua
@@ -3,6 +3,10 @@ local Job = require('plenary.job')
 
 local api = vim.api
 
+local function follow_link()
+	log.info("follow link")
+end
+
 local function edit(url)
 	local bufnr = vim.api.nvim_get_current_buf()
 	api.nvim_buf_set_name(bufnr, url)
@@ -18,11 +22,17 @@ local function edit(url)
 			api.nvim_buf_set_option(bufnr, 'modifiable', true)
 			api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
 			api.nvim_buf_set_option(bufnr, 'modifiable', false)
+			api.nvim_buf_set_option(bufnr, 'filetype', 'gemtext')
+
+			api.nvim_buf_set_keymap(bufnr, 'n', '<cr>', '<cmd>lua require("gmni").follow_link()<cr>', { silent = true })
+			api.nvim_buf_set_keymap(bufnr, 'n', '<tab>', '/^=><cr><cmd>noh<cr>', { silent = true })
+			api.nvim_buf_set_keymap(bufnr, 'n', '<s-tab>', '?^=><cr><cmd>noh<cr>', { silent = true })
 		end),
 	}):start()
 end
 
 return {
 	edit = edit,
+	follow_link = follow_link,
 }
 
