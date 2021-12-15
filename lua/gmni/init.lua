24c4dd31a7914932b70b90dd6c3f8e9962e6becf
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-12-16 00:11:55 +0100

    path -> url

--- lua/gmni/init.lua
+++ lua/gmni/init.lua
@@ -3,17 +3,16 @@ local Job = require('plenary.job')
 
 local api = vim.api
 
-local function edit(path)
-	log.info("BufReadCmd: ", path)
-
+local function edit(url)
 	local bufnr = vim.api.nvim_get_current_buf()
-	api.nvim_buf_set_name(bufnr, path)
+	api.nvim_buf_set_name(bufnr, url)
 
 	Job:new({
 		command = 'gmni',
-		args = { '-j', 'always', path },
+		args = { '-j', 'always', url },
 		on_exit = vim.schedule_wrap(function(j, status)
 			log.info("Status: ", status)
+			log.info("Path: ", url)
 			local contents = j:result()
 
 			api.nvim_buf_set_option(bufnr, 'modifiable', true)
