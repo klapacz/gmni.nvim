f5d47a55b024027a4ae90b5b1e525c4e3b252a60
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-12-17 07:50:12 +0100

    trust popup menu

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ WIP
 use {
 	"https://git.sr.ht/~kornellapacz/gmni.nvim",
 	rocks = { "luasocket" },
+	requires = {
+		'MunifTanjim/nui.nvim',
+		'nvim-lua/plenary.nvim',
+	},
 }
 ```
 
--- lua/gmni/init.lua
+++ lua/gmni/init.lua
@@ -1,3 +1,5 @@
+local trust_policy, request
+
 local log = require('gmni.log')
 local Job = require('plenary.job')
 local urltools = require('socket.url')
@@ -50,7 +52,61 @@ local function follow_link()
 	goto_link(segments[1])
 end
 
-local function request(url, kwargs)
+local Menu = require("nui.menu")
+
+local popup_options = {
+	relative = "cursor",
+	position = {
+		row = 0,
+		col = 0,
+	},
+	border = {
+		style = "rounded",
+		highlight = "FloatBorder",
+		text = {
+			top = "Trust?",
+			top_align = "center",
+		},
+	},
+	highlight = "Normal:Normal",
+}
+
+function trust_policy(bufnr, url, message)
+	helpers.load_to_buf(bufnr, { message[1], message[2], "" })
+	api.nvim_win_set_cursor(0, {3, 0})
+
+	local function callback (item)
+		if item == nil or item.text == "exit" then
+			api.nvim_buf_delete(bufnr, {})
+			return
+		end
+
+		request(url, { trust = item.text })
+	end
+
+	-- HACK: timer is needed to open menu in proper location
+	vim.fn.timer_start(5, function ()
+		Menu(popup_options, {
+			relative = "cursor",
+			lines = {
+				Menu.item("always"),
+				Menu.item("once"),
+				Menu.item("exit"),
+			},
+			keymap = {
+				focus_next = { "j", "<tab>" },
+				focus_prev = { "k", "<s-tab>" },
+				close = { "<Esc>" },
+				submit = { "<CR>" },
+			},
+			min_width = 20,
+			on_close = callback,
+			on_submit = callback,
+		}):mount()
+	end)
+end
+
+function request(url, kwargs)
 	kwargs = kwargs or {}
 
 	local args = { '-iN' }
@@ -72,9 +128,7 @@ local function request(url, kwargs)
 		on_exit = vim.schedule_wrap(function(job, exit_code)
 			spinner.stop(bufnr)
 			if exit_code == 6 then
-				local stderr_result = job:stderr_result()
-				local option = vim.fn.input("Trust " .. stderr_result[2] .. "? (always/once): ")
-				request(url, { trust = option })
+				trust_policy(bufnr, url, job:stderr_result())
 				return
 			end
 
